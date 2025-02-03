a24ad969050e211e62b17fcc71a1d0f01bcf15a9
Volodymyr Andriienko <vandrjios@gmail.com>
2025-02-03 17:54:23 +0200

    Gesture issue: `.updating` block is not called on `LongPressGesture`.

---
+++ Bugs/GestureIssueLongPressUpdatingNotCalled/MRE.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  MRE
+//
+//  Created by VAndrJ on 2/3/25.
+//
+
+import SwiftUI
+import Observation
+
+/// Gesture issue: `.updating` block is not called on `LongPressGesture`.
+/// https://developer.apple.com/documentation/swiftui/longpressgesture example
+struct ContentView: View {
+    @GestureState private var isDetectingLongPress = false
+    @State private var completedLongPress = false
+
+
+    var longPress: some Gesture {
+        LongPressGesture(minimumDuration: 3)
+            .updating($isDetectingLongPress) { currentState, gestureState,
+                transaction in
+                gestureState = currentState
+                transaction.animation = Animation.easeIn(duration: 2.0)
+            }
+            .onEnded { finished in
+                self.completedLongPress = finished
+            }
+    }
+
+
+    var body: some View {
+        Circle()
+            .fill(self.isDetectingLongPress ?
+                  Color.red :
+                    (self.completedLongPress ? Color.green : Color.blue))
+            .frame(width: 100, height: 100, alignment: .center)
+            .gesture(longPress)
+    }
+}
+
---
+++ Bugs/GestureIssueLongPressUpdatingNotCalled/README.md
@@ -0,0 +1,36 @@
+## Problem
+
+
+Gesture issue: `.updating` block is not called on `LongPressGesture`.
+
+
+## Environment
+
+
+- Xcode 16.0-16.2 (current; check on future versions).
+- iOS 18.0-18.2 (current; check on future versions).
+- Swift 5/6.
+
+
+## Solution / Workaround
+
+
+- Build with Xcode 15.4.
+- Use different gestures.
+
+
+## Demo
+
+
+A video demonstrating how it behaves on iOS 18 and on iOS 17.
+
+
+upload video.
+
+
+## Additions
+
+
+Known issue. Shoul be fixed in future releases [FB16430342](https://feedbackassistant.apple.com/feedback/16430342)
+
+

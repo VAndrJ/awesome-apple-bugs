5794e73cd7735d67b21db474a05db7f29b66e6c0
VAndrJ <vandrjios@gmail.com>
2025-02-03 17:01:48 +0200

    The button action is not canceled during scrolling in a sheet and is executed after scrolling completes. (#6)
    * The button action is not canceled during scrolling in sheet and is executed after scrolling completes.
    
    * Demo.

---
+++ Bugs/ButtonActionNotCancelledDuringScrollInSheet/MRE.swift
@@ -0,0 +1,34 @@
+//
+//  SceneDelegate.swift
+//  MRE
+//
+//  Created by VAndrJ on 2/3/25.
+//
+
+import SwiftUI
+
+/// The button action is not canceled during scrolling in a sheet and is executed after scrolling completes.
+struct ContentView: View {
+    @State private var isSheetPresented = false
+
+    var body: some View {
+        Button("Open sheet") {
+            isSheetPresented = true
+        }
+        .sheet(isPresented: $isSheetPresented) {
+            ScrollView {
+                LazyVStack(spacing: 16) {
+                    ForEach(0..<30, id: \.self) { index in
+                        Button {
+                            print("\(index) button action")
+                        } label: {
+                            Text("\(index) button")
+                                .frame(maxWidth: .infinity, idealHeight: 64)
+                        }
+                        .buttonStyle(BorderedProminentButtonStyle())
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Bugs/ButtonActionNotCancelledDuringScrollInSheet/README.md
@@ -0,0 +1,34 @@
+## Problem
+
+
+The button action is not canceled during scrolling in a sheet and is executed after scrolling completes.
+
+
+## Environment
+
+
+- Xcode 16.0-16.2 (current; check on future versions).
+- iOS 18.0-18.2 (current; check on future versions).
+- Swift 5/6.
+
+
+## Solution / Workaround
+
+
+Build in Xcode 15.4.
+
+
+## Demo
+
+
+Video showing how it works on iOS 17 and iOS 18.
+
+
+https://github.com/user-attachments/assets/028762ed-958b-485a-9eb8-bc108245a920
+
+
+## Additions
+
+
+This is most likely related to other gesture issues that were introduced in Xcode 16+iOS 18.
+

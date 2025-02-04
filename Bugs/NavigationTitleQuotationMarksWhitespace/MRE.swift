b4cec9b173a652c7007f2739f5da6a451b1e7f93
Volodymyr Andriienko <vandrjios@gmail.com>
2025-02-04 14:30:50 +0200

    Navigation title is displayed in quotation marks `" "` if a whitespace is specified as the title.

---
+++ Bugs/NavigationTitleQuotationMarksWhitespace/MRE.swift
@@ -0,0 +1,64 @@
+//
+//  SceneDelegate.swift
+//  MRE
+//
+//  Created by VAndrJ on 2/3/25.
+//
+
+import UIKit
+
+/// Navigation title is displayed in quotation marks `" "` if a whitespace is specified as the title.
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+
+        window = UIWindow(windowScene: scene)
+        let navController = UINavigationController(rootViewController: FirstController())
+        window?.rootViewController = navController
+        window?.makeKeyAndVisible()
+    }
+}
+
+final class FirstController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = " "
+
+        let button = UIButton(type: .system)
+        button.setTitle("Next", for: .normal)
+        button.addAction(
+            UIAction(handler: { [weak self] _ in
+                self?.navigationController?.pushViewController(SecondController(), animated: true)
+            }),
+            for: .touchUpInside
+        )
+        button.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(button)
+        NSLayoutConstraint.activate([
+            button.topAnchor.constraint(equalTo: view.topAnchor),
+            button.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            button.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            button.rightAnchor.constraint(equalTo: view.rightAnchor),
+        ])
+    }
+}
+
+final class SecondController: BaseViewController {}
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+}
+
---
+++ Bugs/NavigationTitleQuotationMarksWhitespace/README.md
@@ -0,0 +1,36 @@
+## Problem
+
+
+Navigation title is displayed in quotation marks `" "` if a whitespace is specified as the title.
+
+
+## Environment
+
+
+- Xcode 16.
+- iOS 18.0+.
+- iPadOS 18.0+.
+- Swift 5/6.
+
+
+## Solution / Workaround
+
+
+Set `title = nil` programmatically.
+
+
+## Demo
+
+
+Comparison images on iOS 18 and earlier storyboard/code.
+
+
+![storyboard](https://raw.githubusercontent.com/VAndrJ/awesome-apple-bugs/master/Bugs/NavigationTitleQuotationMarksWhitespace/Resources/storyboard.jpeg)
+![code](https://raw.githubusercontent.com/VAndrJ/awesome-apple-bugs/master/Bugs/NavigationTitleQuotationMarksWhitespace/Resources/code.jpeg)
+
+
+## Additions
+
+
+Not a bug. New behavior.
+
